b3ca0c6544849c463282f3494ff5dd8fecceff67
Kyoka <kyong@fukagawamiyuki-no-MacBook-Pro.local>
2016-03-06 13:58:45 +0900

    すべての

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //これにjQuery書く
+$(function(){
+	console.log(111)
+	$('.lung-hover').hover(
+    function() {
+      $(this).find('.lung-content').addClass('lung-active');
+    },
+    function() {
+      $(this).find('.lung-content').removeClass('lung-active'); 
+    }
+  );
+});
